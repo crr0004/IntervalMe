6019204c4cf5fe0d80602e1311cfcf329b023b87
Christopher Rhodes <crr0004@gmail.com>
2018-04-24 08:58:44 +0900

    Initial project commit

---
+++ app/src/main/java/io/github/crr0004/intervalme/IntervalSettingsActivity.kt
@@ -0,0 +1,12 @@
+package io.github.crr0004.intervalme
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class IntervalSettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
