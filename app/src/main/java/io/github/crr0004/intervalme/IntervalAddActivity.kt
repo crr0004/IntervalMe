07154d74808c347e5440d6c3d9292f48e5eb8a93
Christopher Rhodes <crr0004@gmail.com>
2018-04-24 09:37:26 +0900

    Layout of IntervalAddActivity now centers buttons. Removed most of dp values from layout

--- app/src/main/java/io/github/crr0004/intervalme/IntervalAddActivity.kt
+++ app/src/main/java/io/github/crr0004/intervalme/IntervalAddActivity.kt
@@ -3,10 +3,10 @@ package io.github.crr0004.intervalme
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class IntervalSettingsActivity : AppCompatActivity() {
+class IntervalAddActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_interveraladd)
     }
 }
