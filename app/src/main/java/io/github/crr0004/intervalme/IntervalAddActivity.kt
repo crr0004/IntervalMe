4be2c93b50970d71fbb6646395a32f335acacb7f
Christopher Rhodes <crr0004@gmail.com>
2018-04-24 11:05:24 +0900

    Added ExpandableListView of listing intervals

--- app/src/main/java/io/github/crr0004/intervalme/IntervalAddActivity.kt
+++ app/src/main/java/io/github/crr0004/intervalme/IntervalAddActivity.kt
@@ -1,12 +1,22 @@
 package io.github.crr0004.intervalme
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class IntervalAddActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_interveraladd)
+
+        (findViewById<View>(R.id.goToListBtn)).setOnClickListener {
+            val intent = Intent(this, IntervalListActivity::class.java)
+            startActivity(intent)
+
+        }
     }
+
+
 }
---
+++ app/src/main/java/io/github/crr0004/intervalme/IntervalListActivity.kt
@@ -0,0 +1,15 @@
+package io.github.crr0004.intervalme
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ExpandableListView
+
+class IntervalListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_interval_list)
+        val intervalListView = findViewById<ExpandableListView>(R.id.intervalsExpList)
+
+    }
+}
