e6755f79bc8b4328185393f9db6717b0f961222c
Chris R <crr0004@gmail.com>
2018-08-23 23:44:51 +1000

    Added duration modifier for generating intervals

--- app/src/main/java/io/github/crr0004/intervalme/database/IntervalData.kt
+++ app/src/main/java/io/github/crr0004/intervalme/database/IntervalData.kt
@@ -39,11 +39,11 @@ data class IntervalData(@PrimaryKey(autoGenerate = true) var id: Long=0,
 
             return returnValue
         }
-        fun generate(amount: Int, intervalParent: IntervalData?,startGroupPosition: Int = 0): Array<IntervalData?> {
+        fun generate(amount: Int, intervalParent: IntervalData?,startGroupPosition: Int = 0, durationMod: Long = 100L): Array<IntervalData?> {
             val returnValue = arrayOfNulls<IntervalData>(amount)
             for(i in 0 until returnValue.size){
                 returnValue[i] = IntervalData(label = UUID.randomUUID().toString(), group = intervalParent!!.group, ownerOfGroup = false,groupPosition = startGroupPosition + i.toLong())
-                returnValue[i]!!.duration= (Math.random()*100L).toLong()
+                returnValue[i]!!.duration= (Math.random()*durationMod).toLong()
             }
 
             return returnValue
